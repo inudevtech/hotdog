127069e35ebcfa91f55b922745792f54e94e9d62
Riku Ueda <riku.ueda@outlook.jp>
2022-08-20 13:11:12 +0900

    firebaseをbuild時に作動させないように エラーを吐くため

--- util/firebase/firebase.ts
+++ util/firebase/firebase.ts
@@ -6,6 +6,10 @@ const config = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 };
 
-const firebaseApp = initializeApp(config);
+let firebaseApp;
+if (config.apiKey && config.authDomain) {
+  firebaseApp = initializeApp(config);
+}
+
 const auth = getAuth(firebaseApp);
 export default auth;
