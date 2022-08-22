ee846332fc116f113011a7401564b1fee3c76dd0
Riku Ueda <riku.ueda@outlook.jp>
2022-08-22 12:53:50 +0900

    ログインしていないときにログインを催促するメッセージを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { IWithGoogleReCaptchaProps, withGoogleReCaptcha } from 'react-google-rec
 import Dropzone from 'react-dropzone';
 import ShowFile from '../components/ShowFile';
 import Header from '../components/Header';
+import { AccountContext } from './_app';
 
 interface indexProps {
   file: ReactElement[],
@@ -59,40 +60,61 @@ class index extends Component<{}, indexProps> {
     return (
       <Dropzone onDrop={this.onDrop}>
         {({ getRootProps, getInputProps, isDragActive }) => (
-          <>
-            <Header />
-            <div className="flex justify-center items-center h-screen flex-col">
-              <div className="shadow-xl p-5 flex flex-col md:flex-row-reverse gap-2 lg:w-3/4 w-full xl:w-1/2 min-h-[400px] border border-slate-300 rounded-xl">
-                <div className="basis-1/3 flex flex-col">
+          <AccountContext.Consumer>
+            {(value) => (
+              <>
+                <Header />
+                <div className="flex justify-center items-center h-screen flex-col">
                   <div
-                    {...getRootProps()}
-                    className={`border-dashed border-2 rounded-md flex justify-center items-center p-5 flex-auto ${
-                      isDragActive ? 'border-blue-600 bg-blue-100' : 'border-slate-600'}`}
+                    className="shadow-xl p-5 flex flex-col md:flex-row-reverse gap-2 lg:w-3/4 w-full xl:w-1/2 min-h-[400px] border border-slate-300 rounded-xl"
                   >
-                    <input {...getInputProps()} />
-                    <p>
-                      ファイルをドラッグ&ドロップ
-                    </p>
+                    <div className="basis-1/3 flex flex-col">
+                      <div
+                        {...getRootProps()}
+                        className={`border-dashed border-2 rounded-md flex justify-center items-center p-5 flex-auto ${isDragActive ? 'border-blue-600 bg-blue-100' : 'border-slate-600'}`}
+                      >
+                        <input {...getInputProps()} />
+                        <p>
+                          ファイルをドラッグ&ドロップ
+                        </p>
+                      </div>
+                      <p className="text-center text-2xl">or</p>
+                      <label
+                        htmlFor="upload"
+                        className="transition p-2 border border-slate-300 rounded-md hover:shadow-lg hover:border-slate-500 block w-full text-center"
+                        onChange={this.fileSelected.bind(this)}
+                      >
+                        <FontAwesomeIcon icon={faUpload} className="pr-2" />
+                        ファイルをアップロード
+                        <input type="file" id="upload" className="hidden" multiple />
+                      </label>
+                    </div>
+                    <div className="border-r border-t border-slate-200 border-2" />
+                    <div
+                      className={`flex gap-2 basis-2/3 flex-col ${file.length === 0 ? ' items-center justify-center' : ''}`}
+                    >
+                      {file.length !== 0 ? file
+                        : (
+                          <FontAwesomeIcon
+                            icon={faFolder}
+                            className="h-1/2 w-1/2 md:h-1/2 md:w-1/2"
+                            color="rgb(226 232 240)"
+                          />
+                        )}
+                    </div>
                   </div>
-                  <p className="text-center text-2xl">or</p>
-                  <label
-                    htmlFor="upload"
-                    className="transition p-2 border border-slate-300 rounded-md hover:shadow-lg hover:border-slate-500 block w-full text-center"
-                    onChange={this.fileSelected.bind(this)}
-                  >
-                    <FontAwesomeIcon icon={faUpload} className="pr-2" />
-                    ファイルをアップロード
-                    <input type="file" id="upload" className="hidden" multiple />
-                  </label>
+                  {value.AccountState == null ? (
+                    <div
+                      className="bg-yellow-200/[.6] m-3 p-2 rounded border-2 border-yellow-300 lg:w-3/4 w-full xl:w-1/2"
+                    >
+                      <p className="text-xl">ログインせずにホットドッグを使用しています</p>
+                      <p>ログインをすると様々な特典を受け取れます</p>
+                    </div>
+                  ) : null}
                 </div>
-                <div className="border-r border-t border-slate-200 border-2" />
-                <div className={`flex gap-2 basis-2/3 flex-col ${file.length === 0 ? ' items-center justify-center' : ''}`}>
-                  {file.length !== 0 ? file
-                    : <FontAwesomeIcon icon={faFolder} className="h-1/2 w-1/2 md:h-1/2 md:w-1/2" color="rgb(226 232 240)" />}
-                </div>
-              </div>
-            </div>
-          </>
+              </>
+            )}
+          </AccountContext.Consumer>
         )}
       </Dropzone>
     );
